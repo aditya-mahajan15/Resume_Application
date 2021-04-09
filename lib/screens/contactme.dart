15cef0e8248e3c3587b46281bca202abb1edadce
Rakshitsaini-o-src <rakshit.53-cse-18@mietjammu.in>
2021-04-09 18:11:48 +0530

    Updated contactme page

--- lib/screens/contactme.dart
+++ lib/screens/contactme.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:sample/shared/drawer.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ContactMeScreen extends StatelessWidget {
+
+  launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url, forceWebView: true);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,6 +21,28 @@ class ContactMeScreen extends StatelessWidget {
         leading: null,
       ),
       drawer: MainDrawer(),
+      body: Container(
+        child: Center(
+          child: Column(
+            children: [
+              GestureDetector(
+                onTap: (){
+                  const url = 'https://github.com/aditya-mahajan15';
+                  launchURL(url);
+                },
+                child: Container(
+                  margin: EdgeInsets.only(top: 20),
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage("assets/github.png"),
+                    radius: 50,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
+
